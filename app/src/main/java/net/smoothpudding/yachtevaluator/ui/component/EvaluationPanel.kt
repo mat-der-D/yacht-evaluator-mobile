f5b95bacdaf4d7a50ca4080db8159d918d341611
Smooth Pudding <dummy@address>
2026-01-30 11:09:27 +0900

    [fix] Show the low-most element of the evaluate panel

--- app/src/main/java/net/smoothpudding/yachtevaluator/ui/component/EvaluationPanel.kt
+++ app/src/main/java/net/smoothpudding/yachtevaluator/ui/component/EvaluationPanel.kt
@@ -18,12 +18,15 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -145,6 +148,7 @@ fun EvaluationPanel(
                                 topEnd = PANEL_CORNER_RADIUS
                             )
                         )
+                        .windowInsetsPadding(WindowInsets.navigationBars)
                         .clickable(
                             onClick = { /* Consume click to prevent backdrop dismissal */ },
                             indication = null,
